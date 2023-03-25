37b317235a11d5e1789d166041c768887c2f6e54
Titouan Guiochet <22703571+PeuTit@users.noreply.github.com>
2023-03-25 16:00:51 +0100

    Add test case

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 fn main() {
-    println!("Hello, world!");
-    println!("YO");
-    println!("salut");
+    println!("{} Hello, world! {}", "FDP", truthy());
+}
+
+fn truthy() -> bool {
+    return true;
+}
+
+#[cfg(test)]
+mod test {
+    use super::truthy;
+
+    #[test]
+    fn test_truthy() {
+        assert_eq!(true, truthy());
+    }
+
+    #[test]
+    fn test_untruthy() {
+        assert_ne!(false, truthy());
+    }
 }
