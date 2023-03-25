ab0331e008b7233a3ac0d4bc8da5751b6a8e36f8
Titouan Guiochet <22703571+PeuTit@users.noreply.github.com>
2023-03-25 19:40:41 +0100

    Guessing number game

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("{} Hello, world! {}", "FDP", truthy());
+    println!("Guess the number!");
+
+    let random_number = rand::thread_rng().gen_range(0..=100);
+
+    loop {
+        println!("Please input your guess:");
+
+        let mut guess = String::new();
+        let msg = "No guess";
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect(msg);
+
+        println!("You guessed: {}", guess);
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&random_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Equal!");
+                break;
+            }
+        }
+    }
 }
 
 fn truthy() -> bool {
